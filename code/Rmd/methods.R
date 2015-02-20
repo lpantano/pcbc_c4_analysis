fb08547dcd4d6f77645da1ca49d98b2f1977cdd0
Lorena <lorena.pantano@gmail.com>
2015-02-20 14:02:51 -0500

    temporal changes in examples

--- code/Rmd/methods.R
+++ code/Rmd/methods.R
@@ -1,11 +1,11 @@
 
-get_metadata = function(project){
+get_metadata = function(file_id){
     require(synapseClient)
-    metadataTable <- synGet(project)
+    metadataTable <- synGet(file_id)
 
     colsToUse <- c("UID", "C4_Cell_Line_ID", "bad_lines", "pass_qc", "SampleType", "Cell_Type", "Cell_Line_Type",
                    "Cell_Type_of_Origin", "Cell_Line_of_Origin", "Tissue_of_Origin", "Reprogramming_Vector_Type",
-                   "Reprogramming_Gene_Combination", "C4_Karyotype_Result", "High_Confidence_Donor_ID")
+                   "Reprogramming_Gene_Combination", "C4_Karyotype_Result", "High_Confidence_Donor_ID","Diffname_short","Donor_Life_Stage")
     q <- sprintf("SELECT * FROM %s", metadataTable@properties$id)
 
     metadata <- synTableQuery(q)@values
