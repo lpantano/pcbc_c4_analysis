a206002023f8786c3e8d333fe5a42ea076b7fe41
Lorena <lorena.pantano@gmail.com>
2015-07-09 15:59:58 -0400

    fix some problems with other R versions

--- code/R/lib/clean_mirna.R
+++ code/R/lib/clean_mirna.R
@@ -1,7 +1,7 @@
 clean_table = function(what, mat, meta_clean){
   i = meta_clean %>% filter(Diffname_short %in% what)
   m = mat[,i$UID]
-  
+
   d = data.frame(row.names = colnames(m), cov=colnames(m))
   dds = DESeqDataSetFromMatrix(m,colData = d,design = ~ 1)
   dds = estimateSizeFactors(dds)
@@ -20,15 +20,15 @@ clean_and_sync_mirna_tables = function(metadata, mat, reads, collapse=FALSE){
   # join meso-15 and meso-30
   stages =  gsub("-5","5",gsub("-30","LATE",gsub("-15","LATE",metadata$Diffname_short)))
   metadata$Diffname_short = as.character(stages)
-  
+
   meta_clean = metadata[colnames(mat), ]
   meta_clean[meta_clean == 'N/A'] = NA
   meta_clean[is.na(meta_clean)] = 'unk'
   meta_clean[meta_clean == ''] = 'unk'
-  
+
   meta_clean = meta_clean[keep,]
   mat = mat[,keep]
-  
+
   keep_meta = meta_clean$Diffname_short %in% c("DE", "SC", "ECTO", "MESO5", "MESOLATE", "EB")
   meta_clean = meta_clean[keep_meta,]
   mat = mat[,keep_meta]
@@ -37,15 +37,17 @@ clean_and_sync_mirna_tables = function(metadata, mat, reads, collapse=FALSE){
                              unique(meta_clean$Diffname_short),
                              function(state){
                                cols = row.names(meta_clean[meta_clean$Diffname_short==state,])
-                               PROCESSED_COUNTS = getGeneFilteredGeneExprMatrix(mat[,cols], 
-                                                                                MIN_GENE_CPM=5,
-                                                                                MIN_SAMPLE_PERCENT_WITH_MIN_GENE_CPM=0.9)
+                               PROCESSED_COUNTS = getGeneFilteredGeneExprMatrix(mat[,cols],
+                                                                                MIN_GENE_CPM=10,
+                                                                                MIN_SAMPLE_PERCENT_WITH_MIN_GENE_CPM=0.7,
+                                                                                verbose=TRUE)
                              })
-  names(PROCESSED_COUNTS) = unique(meta_clean$Diffname_short)                             
+  names(PROCESSED_COUNTS) = unique(meta_clean$Diffname_short)
+  sapply(PROCESSED_COUNTS, function(x) print(x$plotHist))
   if (collapse){
     keep = unique(unlist(sapply(PROCESSED_COUNTS, function(x) x$filteredExprMatrix$genes[,1])))
     PROCESSED_COUNTS = list( filteredExprMatrix=DGEList(mat[keep, ], genes=row.names(mat[keep,])) )
   }
-  
+
   return(list(count=PROCESSED_COUNTS, metadata=meta_clean))
 }
